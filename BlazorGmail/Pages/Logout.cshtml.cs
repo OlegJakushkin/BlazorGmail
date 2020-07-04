aeeb764f647b45a9198ba319e8cf307e3e795629
moussa taleb <moussa.taleb@hotmail.com>
2020-07-04 12:18:17 +0200

    Add project files.

---
+++ BlazorGmail/Pages/Logout.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BlazorGmail.Pages
+{
+    public class LogoutModel : PageModel
+    {
+        //public void OnGet()
+        //{
+
+        //}
+
+        public string ReturnUrl { get; private set; }
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+            // Clear the existing external cookie
+            try
+            {
+                await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            }
+            catch (Exception ex)
+            {
+                string error = ex.Message;
+            }
+            return LocalRedirect("/");
+        }
+    }
+}
